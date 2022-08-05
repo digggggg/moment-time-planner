d8856f86dd3316c7f4f7edfc6f74e1bbcc24923a
Jondo <chet9880@gmail.com>
2022-08-05 00:02:34 -0400

    forgot to add what item to pull on local storage

--- script.js
+++ script.js
@@ -3,24 +3,22 @@ var hour = moment().hour()
 
 $('.time-block').each(function (){
     var blockHour = parseInt($(this).attr("id"))
+    console.log(blockHour)
 
     if (blockHour < hour){
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
-    }
-
-    if (blockHour === hour){
+    }else if (blockHour === hour){
         $(this).removeClass("future");
         $(this).addClass("present");
         $(this).removeClass("past");
-    }
-
-    if (blockHour > hour){
+    }else {
         $(this).addClass("future");
         $(this).removeClass("present");
         $(this).removeClass("past");
     }
+
 })
 
 
@@ -31,26 +29,30 @@ function displayTime(){
 }
 
 function loadSave(){
-    $('#9 .description').val(localStorage.getItem())
-    $('#10 .description').val(localStorage.getItem())
-    $('#11 .description').val(localStorage.getItem())
-    $('#12 .description').val(localStorage.getItem())
-    $('#13 .description').val(localStorage.getItem())
-    $('#14 .description').val(localStorage.getItem())
-    $('#15 .description').val(localStorage.getItem())
-    $('#16 .description').val(localStorage.getItem())
-    $('#17 .description').val(localStorage.getItem())
+    $('#9 .description').val(localStorage.getItem('9'))
+    $('#10 .description').val(localStorage.getItem('10'))
+    $('#11 .description').val(localStorage.getItem('11'))
+    $('#12 .description').val(localStorage.getItem('12'))
+    $('#13 .description').val(localStorage.getItem('13'))
+    $('#14 .description').val(localStorage.getItem('14'))
+    $('#15 .description').val(localStorage.getItem('15'))
+    $('#16 .description').val(localStorage.getItem('16'))
+    $('#17 .description').val(localStorage.getItem('17'))
 }
 
 function init(){
     setInterval(displayTime, 1000);
     hourColor();
+    loadSave();
+
+    $('.savBtn').on('click', function(){
+        var data = $(this).siblings('.description').val()
+        var locData = $(this).parent().attr('id')
+        localStorage.setItem(locData, data)
+        console.log('saved')
+    })
 }
 
-$('.savBtn').on('click', function(){
-    var data = $((this).siblings('.description').val())
-    var locData = "At " + $((this).parent().attr('id'))
-    localStorage.setItem(locData, data)
-})
+
 
 init();
