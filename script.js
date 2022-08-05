c8ae6e305b6437843c583dc8c265473ea2eebbe1
Jondo <chet9880@gmail.com>
2022-08-04 23:40:09 -0400

    save item function

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-
-
-
 function hourColor(){
 
 var hour = moment().hour()
 
-$('.time-block').each
+$('.time-block').each(() => {
+    var blockHour = parseInt($(this).attr("id"))
+    if (blockHour < hour){
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+
+    if (blockHour === hour){
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        $(this).removeClass("past");
+    }
+
+    if (blockHour > hour){
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+    }
+})
 
 
 }
@@ -14,8 +30,19 @@ function displayTime(){
     $('#currentDay').text(moment().format("MMMM Do YYYY, h:mm:ss a"))
 }
 
+function loadSave(){
+    $('#9 .description').val(localStorage.getItem())
+}
+
 function init(){
     setInterval(displayTime, 1000);
     hourColor();
 }
 
+$('.savBtn').on('click', () =>{
+    var data = $((this).siblings('.description').val())
+    var locData = "At " + $((this).parent().attr('id'))
+    localStorage.setItem(locData, data)
+})
+
+init();
